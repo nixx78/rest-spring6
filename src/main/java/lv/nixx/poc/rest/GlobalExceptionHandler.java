b019c1a18ef9d4ae2cb65d29cf4c3b176366fce0
nixx78 <nixx78@gmail.com>
2024-04-05 12:22:33 +0300

    Global exception handler added to handle validation errors

---
+++ src/main/java/lv/nixx/poc/rest/GlobalExceptionHandler.java
@@ -0,0 +1,105 @@
+package lv.nixx.poc.rest;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import lombok.Builder;
+import lombok.Getter;
+import org.apache.commons.text.StringEscapeUtils;
+import org.hibernate.validator.internal.engine.path.NodeImpl;
+import org.hibernate.validator.internal.engine.path.PathImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.*;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handle(MethodArgumentNotValidException e, HttpServletRequest request) {
+
+        BindingResult bindingResult = e.getBindingResult();
+
+        List<FieldValidationResponse> fieldValidationResponse = bindingResult.getFieldErrors()
+                .stream()
+                .map(fieldError ->
+                        FieldValidationResponse.builder()
+                                .index(0)
+                                .field(fieldError.getField())
+                                .value(escapeValue(fieldError.getRejectedValue()))
+                                .message(fieldError.getDefaultMessage())
+                                .build()
+                ).toList();
+
+        return createResponseEntity(request, fieldValidationResponse);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> handle(ConstraintViolationException e, HttpServletRequest request) {
+
+        Set<ConstraintViolation<?>> constraintViolations = e.getConstraintViolations();
+
+        Collection<FieldValidationResponse> fieldValidations = constraintViolations.stream()
+                .map(t -> {
+                    PathImpl p = (PathImpl) t.getPropertyPath();
+                    NodeImpl leafNode = p.getLeafNode();
+                    return FieldValidationResponse.builder()
+                            .index(leafNode.getIndex())
+                            .field(leafNode.getName())
+                            .value(escapeValue(leafNode.getValue()))
+                            .message(t.getMessage())
+                            .build();
+                }).toList();
+
+        return createResponseEntity(request, fieldValidations);
+    }
+
+    private String escapeValue(Object v) {
+        if (v == null) {
+            return null;
+        }
+
+        if (v instanceof String) {
+            return StringEscapeUtils.escapeHtml4((String) v);
+        }
+        return v.toString();
+    }
+
+    private static ResponseEntity<Object> createResponseEntity(HttpServletRequest request, Collection<FieldValidationResponse> fieldValidationResponse) {
+        return ResponseEntity.badRequest().body(new ValidationResponse(request.getRequestURI(), request.getMethod(), fieldValidationResponse));
+    }
+
+    @Getter
+    static class ValidationResponse {
+        private final String path;
+        private final String httpMethod;
+
+        private final Map<Integer, List<FieldValidationResponse>> fieldsResponse;
+
+        ValidationResponse(String path, String httpMethod, Collection<FieldValidationResponse> fieldsResponse) {
+            this.path = path;
+            this.httpMethod = httpMethod;
+            this.fieldsResponse = fieldsResponse.stream()
+                    .collect(groupingBy(FieldValidationResponse::getIndex, TreeMap::new, toList()));
+        }
+    }
+
+    @Getter
+    @Builder
+    static class FieldValidationResponse {
+        @JsonIgnore
+        private final int index;
+        private final String field;
+        private final String value;
+        private final String message;
+    }
+
+}
--- src/main/java/lv/nixx/poc/rest/controller/PersonController.java
+++ src/main/java/lv/nixx/poc/rest/controller/PersonController.java
@@ -1,6 +1,8 @@
 package lv.nixx.poc.rest.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lv.nixx.poc.rest.model.NewPersonRequest;
 import lv.nixx.poc.rest.model.PersonDTO;
 import lv.nixx.poc.rest.model.UpdatePersonRequest;
@@ -22,23 +24,27 @@ public class PersonController {
         this.personService = personService;
     }
 
+    @Operation(description = "Add new Person")
     @PostMapping("/person")
     public PersonDTO addPerson(@RequestBody @Valid NewPersonRequest request) {
         return personService.addPerson(request);
     }
 
+    @Operation(description = "Update new Person")
     @PutMapping("/person")
     public PersonDTO updatePerson(@RequestBody @Valid UpdatePersonRequest request) {
         return personService.update(request);
     }
 
+    @Operation(description = "Get list with all persons")
     @GetMapping("/person")
     public Collection<PersonDTO> getPersons() {
         return personService.getAllPersons();
     }
 
+    @Operation(description = "Delete person")
     @DeleteMapping("/person")
-    public PersonDTO deletePerson(Long id) {
+    public PersonDTO deletePerson(@RequestParam @Valid @Min(value = 1, message = "Id must be greater than zero") Long id) {
         return personService.delete(id);
     }
 
